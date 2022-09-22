5c29243069e5ac7cb55bfdebd1d2323cd6f2c080
MSpilari <mb.spilari@hotmail.com>
2022-09-22 12:18:00 -0300

    Criando o controller de adicionar um novo barber

---
+++ src/controllers/AddBarber.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express'
+import { prismaClient } from '../prismaClient'
+import { v2 as cloudinary } from 'cloudinary'
+import { bufferToDataURI } from '../utils/bufferToDataURI'
+
+const AddBarber = async (req: Request, res: Response) => {
+    const barberImage = bufferToDataURI(req)?.content
+    const { name, password } = req.body
+
+    if (barberImage) {
+        cloudinary.uploader
+            .upload(barberImage, { folder: 'barbers avatar' })
+            .then((result) => {
+                const { url } = result
+                return res.status(201).json({
+                    name,
+                    password,
+                    avatarURL: url
+                })
+            })
+            .catch((err) => res.status(400).json({ err }))
+    }
+}
+
+export { AddBarber }
