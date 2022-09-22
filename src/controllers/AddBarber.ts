8436c5d6875d35c66c73fe5f62f17963639ea22e
MSpilari <mb.spilari@hotmail.com>
2022-09-22 17:45:37 -0300

    Upload de foto para cloudinary e tratamento de erros

--- src/controllers/AddBarber.ts
+++ src/controllers/AddBarber.ts
@@ -2,23 +2,37 @@ import { Request, Response } from 'express'
 import { prismaClient } from '../prismaClient'
 import { v2 as cloudinary } from 'cloudinary'
 import { bufferToDataURI } from '../utils/bufferToDataURI'
+import { hash } from 'bcrypt'
 
 const AddBarber = async (req: Request, res: Response) => {
     const barberImage = bufferToDataURI(req)
-    const { name, password } = req.body
+    const { email, password } = req.body
+    const hashPassword = await hash(password, 10)
 
-    if (barberImage) {
-        cloudinary.uploader
-            .upload(barberImage, { folder: 'barbers avatar' })
-            .then((result) => {
-                const { url } = result
-                return res.status(201).json({
-                    name,
-                    password,
-                    avatarURL: url
-                })
-            })
-            .catch((err) => res.status(400).json({ err }))
+    const emailAlreadyExists = await prismaClient.barber.findFirst({
+        where: { email }
+    })
+
+    if (emailAlreadyExists)
+        throw new Error('Sorry, this email is already in use.')
+
+    if (barberImage && !emailAlreadyExists) {
+        const avatarPic = await cloudinary.uploader.upload(barberImage, {
+            folder: 'barbers avatar'
+        })
+        const avatarPicURL = avatarPic.url
+        await prismaClient.barber.create({
+            data: {
+                email,
+                password: hashPassword,
+                avatarUrl: avatarPicURL,
+                appointments: '12345'
+            }
+        })
+
+        return res
+            .status(201)
+            .json({ message: 'Barber created successfully !' })
     }
 }
 
