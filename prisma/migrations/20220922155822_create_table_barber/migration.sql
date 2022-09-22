2dc5d5eef119841b144a953e413bf7c6e4d1eeb3
MSpilari <mb.spilari@hotmail.com>
2022-09-22 16:57:58 -0300

    Rodando novas migrations

--- prisma/migrations/20220922155822_create_table_barber/migration.sql
+++ prisma/migrations/20220922155822_create_table_barber/migration.sql
@@ -1,9 +1,15 @@
 -- CreateTable
 CREATE TABLE "Barber" (
     "id" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "avatarUrl" TEXT NOT NULL,
     "appointments" TEXT NOT NULL,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Barber_id_key" ON "Barber"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Barber_email_key" ON "Barber"("email");
