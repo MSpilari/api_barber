8fcbacc6caaaed8a9ad37fd1e5f97bd74bb3ad55
MSpilari <mb.spilari@hotmail.com>
2022-09-28 17:40:41 -0300

    Criando a rota de login

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 import { multerUploads } from '../configs/multer'
 import { AddBarber } from '../controllers/AddBarber'
+import { Login } from '../controllers/Login'
 
 const router = Router()
 
 router.post('/barber', multerUploads.single('barberImage'), AddBarber)
+router.post('/login', Login)
 
 export { router }
