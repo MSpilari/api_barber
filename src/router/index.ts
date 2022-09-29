91c92a0a820e27d27b13f2af79316cffc1ae6b90
MSpilari <mb.spilari@hotmail.com>
2022-09-29 17:19:12 -0300

    Adicionando a rota de deleção de barber

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express'
 import { multerUploads } from '../configs/multer'
 import { AddBarber } from '../controllers/AddBarber'
+import { DeleteBarber } from '../controllers/DeleteBarber'
 import { Login } from '../controllers/Login'
+import { authToken } from '../middlewares/authToken'
 
 const router = Router()
 
 router.post('/barber', multerUploads.single('barberImage'), AddBarber)
 router.post('/login', Login)
+
+router.delete('/barber', authToken, DeleteBarber)
 
 export { router }
