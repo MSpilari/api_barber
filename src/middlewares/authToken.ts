801573d7ce4b61534f1f4b79158a9c4844b0aaf5
MSpilari <mb.spilari@hotmail.com>
2022-10-03 12:39:45 -0300

    Criando o middleware de verificação dos tokens

--- src/middlewares/authToken.ts
+++ src/middlewares/authToken.ts
@@ -1,6 +1,20 @@
 import { NextFunction, Request, Response } from 'express'
 import { verify } from 'jsonwebtoken'
 
-const authToken = (req: Request, res: Response, next: NextFunction) => {}
+const authToken = (req: Request, res: Response, next: NextFunction) => {
+    const fullToken = req.headers.token
+
+    const token = fullToken && fullToken.split(' ')
+
+    const isAValidToken =
+        token &&
+        process.env.JWT_SECRET &&
+        verify(token[1], process.env.JWT_SECRET)
+
+    if (!token || !isAValidToken)
+        return res.status(403).json({ error: 'Token not found/invalid' })
+
+    return next()
+}
 
 export { authToken }
