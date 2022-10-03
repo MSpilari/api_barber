2e2ab3076efe8c3b2ad707f3acb879b6b419f2ef
MSpilari <mb.spilari@hotmail.com>
2022-10-03 12:40:31 -0300

    Deletando um barber e sua foto no cloudinary

--- src/controllers/DeleteBarber.ts
+++ src/controllers/DeleteBarber.ts
@@ -1,5 +1,32 @@
+import { v2 as cloudinary } from 'cloudinary'
 import { Request, Response } from 'express'
+import { decode } from 'jsonwebtoken'
+import { prismaClient } from '../configs/prismaClient'
 
-const DeleteBarber = async (req: Request, res: Response) => {}
+const DeleteBarber = async (req: Request, res: Response) => {
+    const { token } = req.headers
+
+    if (token) {
+        const [_, code] = token.split(' ')
+
+        const barberId = decode(code, { json: true })
+
+        const deletedBarber =
+            barberId &&
+            (await prismaClient.barber.findFirst({
+                where: { id: barberId._id }
+            }))
+
+        deletedBarber &&
+            (await cloudinary.uploader.destroy(deletedBarber.email)) &&
+            (await prismaClient.barber.delete({
+                where: { id: deletedBarber.id }
+            }))
+
+        return res.status(200).json({ message: 'Barber deleted successfully.' })
+    }
+
+    return res.status(403).json({ error: 'Token not found' })
+}
 
 export { DeleteBarber }
